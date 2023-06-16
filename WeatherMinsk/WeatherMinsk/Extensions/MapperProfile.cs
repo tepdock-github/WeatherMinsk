20248f80c172a1b0a1ef1ade31a30a1c27499ddd
tepduck <lksunaylkolodl@gamil.com>
2023-06-16 06:02:34 -0700

    moved git + added mapper profile

---
+++ WeatherMinsk/WeatherMinsk/Extensions/MapperProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using WeatherMinsk.Domain.DTO;
+using WeatherMinsk.Domain.Entities;
+
+namespace WeatherMinsk.Extensions
+{
+    public class MapperProfile : Profile
+    {
+        protected MapperProfile()
+        {
+            CreateMap<WeatherData, WeatherDataDTO>();
+            CreateMap<WeatherDataManipulationDTO, WeatherData>();
+        }
+    }
+}
